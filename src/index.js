b536a2981890c5fa7dac86a73cf0b34c485923a4
Ray <ray.rodz.88@gmail.com>
2018-09-13 11:59:01 -0500

    calendarYearMOnthDAywithBreaks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const eventsUrl = 'http://localhost:3000/api/v1/events'
   const datesUrl = 'http://localhost:3000/api/v1/calendar_dates'
   const eventDatesUrl = 'http://localhost:3000/api/v1/event_dates' 
+  const currentYear = document.getElementById("calendar_year")
+  const currentMonth = document.getElementById("calendar_month")
   const navBar = document.getElementById("nav-bar")
 
   getDates();
@@ -19,11 +21,24 @@ document.addEventListener('DOMContentLoaded', () => {
       let day = new Date(dateData.date)
       let dayOfMonth = day.getDate();
       let dayOfWeek = day.getDay();
+      let month = day.getMonth();
+      let year = day.getFullYear();
       const sundayOfday = (dayOfMonth - dayOfWeek)
       
+      setMonth(month)
+      setYear(year)
       findWeekofSunday(sundayOfday, 1)
     })
    
+  }
+
+  function setMonth(month){
+   const months = {0:"January", 1:"Febuary", 2:"March", 3:"April", 4:"May", 5:"June", 6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"}
+    currentMonth.innerText = months[month]
+  }
+  
+  function setYear(year){
+    currentYear.innerText = year
   }
 
   function findWeekofSunday(sundayOfday, i){
@@ -43,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let tableWeekTr = calendarTable[0].children[0].children[week]
 
     Array.from(tableWeekTr.children).forEach(function(tr){
-      if (day < 32 ){
+      if (day < daysInMonth()+1 ){
         tr.children[0].innerHTML = day
         day++
       }
@@ -53,6 +68,11 @@ document.addEventListener('DOMContentLoaded', () => {
         let nextWeek = (week+1)
         putNumbersOnCaldar(nextWeek, day)
       }
+  }
+
+  function daysInMonth(){
+    const monthDays = {"January":31, "Febuary":28, "March":31, "April":30, "May":31, "June":30, "July":31, "August":31, "September":30, "October":31, "November":30, "December":31}
+    return monthDays[currentMonth.innerText]
   }
     
   
