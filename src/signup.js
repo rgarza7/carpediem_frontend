94f44106eed1c850bd998686c16111ae0f6d1a7c
github username <github email address>
2018-09-14 14:50:33 -0500

    updated login page

---
+++ src/signup.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const userUrl = 'http://localhost:3000/api/v1/users'
+const signUpForm = document.querySelector('#form_signup')
+
+signUpForm.addEventListener('submit', function(event) {
+  
+})
+
+}
+
+
+
+
+
+
+
+}
